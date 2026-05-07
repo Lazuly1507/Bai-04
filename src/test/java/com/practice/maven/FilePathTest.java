bb7874d1f65cc5278cf67f6e0021743aee1f7cc7
Lazuly <nhlazuly@gmail.com>
2026-05-07 16:46:48 +0700

    test work everywhere

--- src/test/java/com/practice/maven/FilePathTest.java
+++ src/test/java/com/practice/maven/FilePathTest.java
@@ -5,13 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FilePathTest {
   @Test
-  void testOnlyWorksOnWindows() {
-    // Dấu \\ chỉ có tác dụng trên Windows
-    // Trên máy bạn (Windows), đường dẫn này sẽ hợp lệ -> True
-    String path = ".\\pom.xml";
-    java.io.File file = new java.io.File(path);
-
-    // Kiểm tra file có tồn tại không
-    assertTrue(file.exists(), "File phải tồn tại trên máy Windows!");
+  void testWorksEverywhere() {
+    // Dùng Paths.get để tự thích nghi với mọi OS
+    java.nio.file.Path path = java.nio.file.Paths.get("pom.xml");
+    assertTrue(java.nio.file.Files.exists(path));
   }
 }
